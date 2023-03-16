3196ac7e95318bb7d0e069f6a5e554ac0a033149
Brock Angelo <brock.angelo@corteva.com>
2023-03-16 15:03:48 -0500

    aliases for pip_unset and pip_set to remove the work-related pip configs

--- aliases/granular.sh
+++ aliases/granular.sh
@@ -10,3 +10,6 @@ alias cdcdt='cd ~/granular/cat/brock_dotfiles'
 # JIRA
 alias sprint='open https://granular.atlassian.net/secure/RapidBoard.jspa\?projectKey\=BCN\&assignee\=557058%3Abc723c28-068c-4558-9d18-ada557631947'
 
+# Removes the pip.conf pointing to Granular's Artifactory for local/personal development off the VPN
+alias pip_unset='rm ~/.pip'
+alias pip_set='ln -s ~/dotfiles/symlinks/pip ~/.pip'
