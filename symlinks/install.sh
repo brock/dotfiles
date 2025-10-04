69d67d18f760edf977329555e8c11265883f4a4e
Brock Angelo <brockangelo@gmail.com>
2025-10-04 08:40:09 -0500

    remove Corteva gitconfig

--- symlinks/install.sh
+++ symlinks/install.sh
@@ -38,12 +38,6 @@ if [[ ! -L ~/.gitconfig ]]; then
     ln -s $DOTFILES/symlinks/gitconfig ~/.gitconfig
 fi
 
-debug "Verifying ~/.gitconfig.corteva symlink"
-if [[ ! -L ~/.gitconfig.corteva ]]; then
-	echo "Symlinking ~/.gitconfig.corteva"
-    ln -s $DOTFILES/symlinks/gitconfig.corteva ~/.gitconfig.corteva
-fi
-
 ##### vim
 debug "Verifying ~/.vimrc symlink"
 if [[ ! -L ~/.vimrc ]]; then
