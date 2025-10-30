726e1ab517a0b081eb77cfeaafaf7ee2b2b0b37c
Brock Angelo <brockangelo@gmail.com>
2025-10-30 09:27:04 -0500

    add readme and help docs. use a read/write account for the shares so I can add and rename files

--- scripts/maintain-smb-shares/maintain-smb-shares.sh
+++ scripts/maintain-smb-shares/maintain-smb-shares.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # mount_shares.sh
 # check if Synology Samba shares are mounted, else remount them
-# The password for the "viewer" account on synologynas has been
-# saved in OSX Keychain by manually mounting through the UI
+# The password for the "brockangelo" account on synologynas has been
+# saved in OSX Keychain by manually mounting through the UI.
 # See the symlinked file in 
 # symlinks/Library/LaunchAgents/com.brockangelo.maintainsmb.plist
 
@@ -14,6 +14,6 @@ do
    # Check if the mount point exists and is a mounted volume
    if ! mount | grep "/Volumes/${i}"; then
        # If not mounted, try to remount it using AppleScript
-       osascript -e "mount volume \"smb://viewer@synologynas/${i}\""
+       osascript -e "mount volume \"smb://brockangelo@synologynas/${i}\""
    fi
 done
