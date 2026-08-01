751bbcc42e1b30f2135d2fdf4af4a7dbaf773ff7
Brock Angelo <brockangelo@gmail.com>
2026-08-01 07:26:27 -0500

    include instructions in local-overrides.sh

--- symlinks/zsh/local-overrides.sh
+++ symlinks/zsh/local-overrides.sh
@@ -1,3 +1,4 @@
+# This file is sourced in ~/.zshrc
 # Changes to this file are ignored by git
 # You can add overrides that only need to exist on the current machine
 # without modifying the default zshrc
